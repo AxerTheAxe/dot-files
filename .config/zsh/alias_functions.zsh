ca3df212549188543e656abcf3c586b520333504
Carter Jamison <AxerTheAxe@proton.me>
2024-03-04 13:44:08 -0700

    Update configurations for ZSH

--- .config/zsh/alias_functions.zsh
+++ .config/zsh/alias_functions.zsh
@@ -4,7 +4,9 @@ cd() { __zoxide_z "$@" }
 mkdir() { /usr/bin/mkdir -p "$@" }
 mkcd() { mkdir -p "$@" && cd "$@" }
 
-rm() { /usr/bin/rm -r "$@" }
+rm() { /usr/bin/rm -rf "$@" }
+
+ls() { /usr/bin/ls --color "$@" }
 
 grep() { /usr/bin/grep --color=auto "$@" }
 
