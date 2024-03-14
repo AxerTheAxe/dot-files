be55d408d550f8272fcf42bbecf30b66a9486990
Carter Jamison <AxerTheAxe@proton.me>
2024-03-13 18:16:56 -0600

    Add hibernate alias for ZSH

--- .config/zsh/alias_functions.zsh
+++ .config/zsh/alias_functions.zsh
@@ -10,6 +10,8 @@ rm() { /usr/bin/rm -rf "$@" }
 
 ls() { /usr/bin/ls --color=auto "$@" }
 
+hibernate() { systemctl suspend }
+
 grep() { /usr/bin/grep --color=auto "$@" }
 
 nohup() { /usr/bin/nohup "$@" > "$HOME"/.cache/nohup.out > /dev/null 2>&1 }
